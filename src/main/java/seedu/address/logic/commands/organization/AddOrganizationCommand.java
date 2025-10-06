7664babf2837dafabb150a787e0950ebd08ee33b
SanguineChameleon <97085374+SanguineChameleon@users.noreply.github.com>
2025-10-06 11:27:38 +0800

    AddOrganizationCommand.java: change command word to add-org

--- src/main/java/seedu/address/logic/commands/organization/AddOrganizationCommand.java
+++ src/main/java/seedu/address/logic/commands/organization/AddOrganizationCommand.java
@@ -20,7 +20,7 @@ import seedu.address.model.organization.Organization;
  */
 public class AddOrganizationCommand extends Command {
 
-    public static final String COMMAND_WORD = "add";
+    public static final String COMMAND_WORD = "add-org";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds an organization to the address book. "
             + "Parameters: "
