5903d4c03918c5a7111368777a71567902fd835a
SanguineChameleon <97085374+SanguineChameleon@users.noreply.github.com>
2025-10-06 11:40:57 +0800

    DeleteOrganizationCommandParser.java: change to parse DeleteOrganizationCommand

--- src/main/java/seedu/address/logic/parser/organization/DeleteOrganizationCommandParser.java
+++ src/main/java/seedu/address/logic/parser/organization/DeleteOrganizationCommandParser.java
@@ -3,28 +3,28 @@ package seedu.address.logic.parser.organization;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 import seedu.address.commons.core.index.Index;
-import seedu.address.logic.commands.DeleteCommand;
+import seedu.address.logic.commands.organization.DeleteOrganizationCommand;
 import seedu.address.logic.parser.Parser;
 import seedu.address.logic.parser.ParserUtil;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
- * Parses input arguments and creates a new DeleteCommand object
+ * Parses input arguments and creates a new DeleteOrganizationCommand object
  */
-public class DeleteOrganizationCommandParser implements Parser<DeleteCommand> {
+public class DeleteOrganizationCommandParser implements Parser<DeleteOrganizationCommand> {
 
     /**
-     * Parses the given {@code String} of arguments in the context of the DeleteCommand
-     * and returns a DeleteCommand object for execution.
+     * Parses the given {@code String} of arguments in the context of the DeleteOrganizationCommand
+     * and returns a DeleteOrganizationCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
-    public DeleteCommand parse(String args) throws ParseException {
+    public DeleteOrganizationCommand parse(String args) throws ParseException {
         try {
             Index index = ParserUtil.parseIndex(args);
-            return new DeleteCommand(index);
+            return new DeleteOrganizationCommand(index);
         } catch (ParseException pe) {
             throw new ParseException(
-                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE), pe);
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteOrganizationCommand.MESSAGE_USAGE), pe);
         }
     }
 
