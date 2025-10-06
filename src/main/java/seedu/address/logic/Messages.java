dfbe59b5b162e54a2512e3975c42537923351dd9
SanguineChameleon <97085374+SanguineChameleon@users.noreply.github.com>
2025-10-06 11:20:16 +0800

    Messages.java: add messages for organization support

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -18,7 +18,13 @@ public class Messages {
     public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The person index provided is invalid";
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
     public static final String MESSAGE_DUPLICATE_FIELDS =
-                "Multiple values specified for the following single-valued field(s): ";
+            "Multiple values specified for the following single-valued field(s): ";
+
+    // === Organization messages ===
+    public static final String MESSAGE_INVALID_ORGANIZATION_DISPLAYED_INDEX =
+            "The organization index provided is invalid";
+    public static final String MESSAGE_ORGANIZATIONS_LISTED_OVERVIEW =
+            "%1$d organizations listed!";
 
     /**
      * Returns an error message indicating the duplicate prefixes.
