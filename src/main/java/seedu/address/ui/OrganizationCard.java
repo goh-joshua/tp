a9ec1e6e65cb969bb167d25840434855b348b296
kellytan <kellytxn@gmail.com>
2025-10-19 04:19:42 +0800

    Change import order

--- src/main/java/seedu/address/ui/OrganizationCard.java
+++ src/main/java/seedu/address/ui/OrganizationCard.java
@@ -1,5 +1,9 @@
 package seedu.address.ui;
 
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.stream.Collectors;
+
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
@@ -8,10 +12,6 @@ import javafx.scene.layout.Region;
 import seedu.address.model.athlete.Athlete;
 import seedu.address.model.contract.Contract;
 import seedu.address.model.organization.Organization;
-
-import java.text.NumberFormat;
-import java.util.Locale;
-import java.util.stream.Collectors;
 
 /**
  * An UI component that displays information of a {@code Organization}.
